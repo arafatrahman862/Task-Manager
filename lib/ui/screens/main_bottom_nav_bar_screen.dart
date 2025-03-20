10f53ff3ffeb67404db3af9c383c6dd7be89b7d6
xtr <99972602+arafatrahman862@users.noreply.github.com>
2025-03-21 02:20:02 +0600

    screens added

--- lib/ui/screens/main_bottom_nav_bar_screen.dart
+++ lib/ui/screens/main_bottom_nav_bar_screen.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:task_manager/ui/screens/cancelled_task_screen.dart';
+import 'package:task_manager/ui/screens/completed_task_screen.dart';
+import 'package:task_manager/ui/screens/new_task_screen.dart';
+import 'package:task_manager/ui/screens/progress_task_screen.dart';
 
 class MainBottomNavBarScreen extends StatefulWidget {
   const MainBottomNavBarScreen({super.key});
@@ -8,10 +12,24 @@ class MainBottomNavBarScreen extends StatefulWidget {
 }
 
 class _MainBottomNavBarScreenState extends State<MainBottomNavBarScreen> {
+  int _selectedIndex = 0;
+  final List<Widget> _screens = const [
+    NewTaskScreen(),
+    CompletedTaskScreen(),
+    CancelledTaskScreen(),
+    ProgressTaskScreen(),
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      body: _screens[_selectedIndex],
       bottomNavigationBar: NavigationBar(
+        selectedIndex: _selectedIndex,
+        onDestinationSelected: (int index) {
+          _selectedIndex = index;
+          setState(() {});
+        },
         destinations: const [
           NavigationDestination(
             icon: Icon(Icons.new_label),
