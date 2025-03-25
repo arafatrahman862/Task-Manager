9e92737dc25d9c0bfb9cc6e12915e2f833c6b764
xtr <99972602+arafatrahman862@users.noreply.github.com>
2025-03-25 15:13:13 +0600

    new task section implemented

--- lib/ui/screens/new_task_screen.dart
+++ lib/ui/screens/new_task_screen.dart
@@ -1,12 +1,35 @@
 import 'package:flutter/material.dart';
+import 'package:task_manager/ui/screens/add_new_task_screen.dart';
 
-class NewTaskScreen extends StatelessWidget {
+class NewTaskScreen extends StatefulWidget {
   const NewTaskScreen({super.key});
 
   @override
+  State<NewTaskScreen> createState() => _NewTaskScreenState();
+}
+
+class _NewTaskScreenState extends State<NewTaskScreen> {
+  @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text('New Task'),
+    return Scaffold(
+      body: const Center(
+        child: Text('New Task'),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _onTapAddAB,
+        child: const Icon(Icons.add),
+        shape: const CircleBorder(),
+      ),
+    );
+  }
+
+  void _onTapAddAB(){
+    Navigator.push(
+      context,
+      MaterialPageRoute(
+        builder: (context) => const AddNewTaskScreen(),
+      ),
     );
   }
 }
+
